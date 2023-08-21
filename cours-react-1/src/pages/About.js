2f17ad93d85b245c6be5979059e649552e021c1d
ixyhoo <ixyhoo@outlook.fr>
2023-08-21 21:54:33 +0200

    I've done route path

--- cours-react-1/src/pages/About.js
+++ cours-react-1/src/pages/About.js
@@ -1,4 +1,4 @@
-onst About = () => {
+const About = () => {
     return (
       <div>
         <h1>A propos</h1>
@@ -19,6 +19,32 @@ onst About = () => {
              distinctio porro totam aliquam vero velit 
              reprehenderit 
             quibusdam ratione neque aperiam amet aspernatur!</p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur 
+                adipisicing elit. Iusto expedita, tempora corrupti rerum,
+                 sed quas repudiandae sapiente quis aperiam adipisci 
+                 placeat similique! Consequatur totam dolores voluptas 
+                 assumenda, vel laudantium vero natus impedit obcaecati 
+                 tenetur veniam doloremque, ipsa temporibus iste quod architecto,
+                  corporis optio. Voluptates omnis veritatis voluptatem molestias 
+                    consectetur quisquam assumenda. A reiciendis tenetur perferendis odio,
+                     ad quidem explicabo sunt numquam aliquid illo veniam id optio sit, 
+                     quia ex placeat. Facere dolorum possimus quos repellendus veritatis 
+                     sint quam voluptate debitis, enim tenetur, illum vitae minima odit 
+                     fuga sed at sequi commodi minus, consectetur adipisci labore 
+                     obcaecati quidem! Cum illo consequatur commodi temporibus quasi
+                      alias enim tenetur, eum dolorem laborum quibusdam ipsa odio dolores
+                       praesentium natus consectetur deleniti placeat ipsum expedita quo 
+                       facilis possimus reiciendis recusandae rem. Cupiditate consequatur 
+                       maxime magni saepe commodi, labore iusto neque error rerum 
+                       temporibus, facere rem praesentium voluptates corporis alias 
+                       esse exercitationem nemo reprehenderit et corrupti, asperiores 
+                       recusandae dolor? Vero cumque ea, doloremque corporis perspiciatis
+                        exercitationem est corrupti alias repudiandae, nihil modi tenetur 
+                        sunt laboriosam sit! Vitae voluptates facere earum modi aliquid 
+                        culpa illo corporis. Temporibus provident unde sunt doloremque, 
+                        veritatis velit amet magni libero, ex totam fugit obcaecati. 
+                Ipsam odit accusantium rerum, optio perspiciatis necessitatibus?</p>
       </div>
     );
   };
