e9a05710445e0dc2014ed5979c7e0cc1d7512015
ixyhoo <ixyhoo@outlook.fr>
2023-08-21 22:54:25 +0200

    ok for today bug routes

--- cours-react-1/src/App.js
+++ cours-react-1/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
-import About from './pages/About';
+
 
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About/>}
       </Routes>
    </BrowserRouter>
    
