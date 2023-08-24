1cc9a697766577a57f8b2c9c95b02b6e01187371
ixyhoo <ixyhoo@outlook.fr>
2023-08-24 23:07:09 +0200

    I done api contries

---
+++ cours-react-1/src/components/Countries.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Countries = () => {
+
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all").then( (res) => console.log(res.data));
+    }, []);
+    return (
+        <div className='countries'>
+            <h1>Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
