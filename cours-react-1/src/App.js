bfbda67fd29e54e7402af481cff1edaa95c33d6d
ixyhoo <ixyhoo@outlook.fr>
2023-08-24 22:03:19 +0200

    Routes

--- cours-react-1/src/App.js
+++ cours-react-1/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 
 
@@ -9,8 +10,10 @@ const App = () => {
   return (
    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>}
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<h1>404</h1>} />
+       
       </Routes>
    </BrowserRouter>
    
