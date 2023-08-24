e0cb889e30dfb433bb55434b8400e76a9e934a0f
ixyhoo <ixyhoo@outlook.fr>
2023-08-24 22:11:31 +0200

    I done navigation

--- cours-react-1/src/pages/About.js
+++ cours-react-1/src/pages/About.js
@@ -1,6 +1,9 @@
+import Navigation from "../components/Navigation";
+
 const About = () => {
     return (
       <div>
+        <Navigation />
         <h1>A propos</h1>
         <br />
         <p>Lorem ipsum dolor sit amet consectetur 
